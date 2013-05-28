456255f7c3fb39c163bf7f3c46ef8048c20d7276
Pepe <pepe@yuilop.com>
2013-05-28 11:58:46 +0200

    Add copy metadata function

--- src/riak_utils.erl
+++ src/riak_utils.erl
@@ -4,7 +4,8 @@
     dict_to_json/1,
     json_to_dict/1,
     date_to_integer_iso8601/1,
-    datetime_to_integer_iso8601/1
+    datetime_to_integer_iso8601/1,
+    copy_metadata/2
 ]).
 
 dict_to_json(Dict) when is_tuple(Dict) ->
@@ -13,8 +14,20 @@ dict_to_json(Dict) when is_tuple(Dict) ->
 json_to_dict(Json) when is_binary(Json) ->
     dict:from_list(jsx:decode(Json)).
 
+-spec date_to_integer_iso8601(Datetime::calendar:date()) -> non_neg_integer().
+
 date_to_integer_iso8601({Y,M,D}) ->
     Y * 10000000000 + M * 100000000 + D * 1000000.
 
-datetime_to_integer_iso8601({datetime,{{Y,M,D},{H,Min,S}}}) ->
+-spec datetime_to_integer_iso8601(Datetime::calendar:datetime()) -> non_neg_integer().
+
+datetime_to_integer_iso8601({datetime, {{Y,M,D},{H,Min,S}}}) ->
     Y * 10000000000 + M * 100000000 + D * 1000000 + H * 10000 + Min * 100 + S.
+
+-spec copy_metadata(Origin::riakc:riak_obj(), Destination::riakc:riak_obj()) -> riakc:riak_obj().
+
+copy_metadata(undefined, Destination) ->
+	Destination;
+copy_metadata(Origin, Destination) ->
+	MD = riakc_obj:get_update_metadata(Origin),
+	riakc_obj:update_metadata(Destination, MD).
