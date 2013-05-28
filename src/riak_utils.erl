aa129ed1f92dfd7285ff26de490d127bc1b4566e
Pepe <pepe@yuilop.com>
2013-05-28 17:36:14 +0200

    Copy secundary indexes

--- src/riak_utils.erl
+++ src/riak_utils.erl
@@ -5,7 +5,7 @@
     json_to_dict/1,
     date_to_integer_iso8601/1,
     datetime_to_integer_iso8601/1,
-    copy_metadata/2
+    copy_secundary_indexes/2
 ]).
 
 dict_to_json(Dict) when is_tuple(Dict) ->
@@ -26,12 +26,12 @@ datetime_to_integer_iso8601({datetime, {{Y,M,D},{H,Min,S}}}) ->
 datetime_to_integer_iso8601({{Y,M,D},{H,Min,S}}) ->
     Y * 10000000000 + M * 100000000 + D * 1000000 + H * 10000 + Min * 100 + S.
 
--spec copy_metadata(Origin::riakc:riak_obj(), Destination::riakc:riak_obj()) -> riakc:riak_obj().
+-spec copy_secundary_indexes(Origin::riakc:riak_obj(), Destination::riakc:riak_obj()) -> riakc:riak_obj().
 
-copy_metadata(undefined, Destination) ->
+copy_secundary_indexes(undefined, Destination) ->
 	Destination;
-copy_metadata(Origin, Destination) ->
-	UMD = riakc_obj:get_update_metadata(Origin),
-    MD = riakc_obj:get_metadata(Origin),
-    NMD = dict:merge(fun(_K, _V1, V2) -> V2 end, MD, UMD),
-	riakc_obj:update_metadata(Destination, NMD).
+copy_secundary_indexes(Origin, Destination) ->
+	MD = riakc_obj:get_update_metadata(Origin),
+    Indexes = riakc_obj:get_secondary_indexes(MD),
+    MD1 = riakc_obj:set_secondary_index(MD, Indexes),
+	riakc_obj:update_metadata(Destination, MD1).
